73de5da452b46611d90c9589ce319b84b2e35dab
Christian Mahnke <mahnke@sub.uni-goettingen.de>
2014-05-31 09:26:28 +0200

    added some unit tests

---
+++ layout/src/test/groovy/de/unigoettingen/sub/be/ous/print/layout/Asc2XmlTest.groovy
@@ -0,0 +1,71 @@
+/*
+ * This file is part of the OUS Print Server, Copyright 2011, 2012 SUB Göttingen
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, see http://www.gnu.org/licenses or write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301 USA.
+ */
+
+package de.unigoettingen.sub.be.ous.print.layout
+/**
+ * Tests for ASC to XML transformations
+ * @author cmahnke
+ */
+
+import groovy.util.logging.Log4j
+import groovy.transform.TypeChecked
+
+import static org.junit.Assert.*
+import de.unigoettingen.sub.be.ous.print.layout.Asc2Xml
+import org.junit.Ignore
+import org.junit.Test
+
+@TypeChecked
+@Log4j
+class Asc2XmlTest {
+    static List<URL> URLS = [Asc2XmlTest.getClass().getResource("/layouts/ous40_layout_001_du.asc"), 
+        Asc2XmlTest.getClass().getResource("/layouts/ous40_layout_001_en.asc")]
+    static List<URL> BROKEN = [Asc2XmlTest.getClass().getResource("/layouts-broken/ous40_layout_001_du.asc"), 
+        Asc2XmlTest.getClass().getResource("/layouts-broken/ous40_layout_001_en.asc")]
+    
+    //Test transformation
+    @Test
+    @TypeChecked
+    void testTransform () {
+        for (asc in URLS) {
+            log.info('Transforming ASC File ' + asc.toString())
+            Asc2Xml a2x = new Asc2Xml(asc)
+            try {
+                a2x.transform()
+            } catch (javax.xml.transform.TransformerException te) {
+                log.error('Transformation failed!')
+                fail(te.getMessage())
+            }
+            log.trace('Result:\n----------------START OF RESULT(' + this.getClass().getName() + ')\n' + a2x.getXML())
+            log.trace('----------------END OF RESULT(' + this.getClass().getName() + ')\n')
+        }
+        
+    }
+    
+    //Test broken files
+    @Test(expected=IllegalStateException.class)
+    void testBroken () {
+        for (asc in BROKEN) {
+          log.info('Testing broken charset ' + asc.toString())
+          Asc2Xml a2x = new Asc2Xml(asc)
+          a2x.transform()
+        }
+    }
+
+}
+
---
+++ layout/src/test/groovy/de/unigoettingen/sub/be/ous/print/layout/Xml2ParserTest.groovy
@@ -0,0 +1,111 @@
+/*
+ * This file is part of the OUS Print Server, Copyright 2011, 2012 SUB Göttingen
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, see http://www.gnu.org/licenses or write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301 USA.
+ */
+
+package de.unigoettingen.sub.be.ous.print.layout
+
+/**
+ * Test class for generating parsers from ASC files
+ * @author cmahnke
+ */
+
+import groovy.util.logging.Log4j
+import javax.xml.transform.Source
+import javax.xml.transform.dom.DOMResult
+import javax.xml.transform.dom.DOMSource
+import javax.xml.transform.stream.StreamSource
+import javax.xml.transform.TransformerFactory
+import org.junit.BeforeClass
+import groovy.transform.TypeChecked
+
+import static org.junit.Assert.*
+import de.unigoettingen.sub.be.ous.print.layout.Xml2Parser
+import de.unigoettingen.sub.be.ous.print.util.LogErrorListener
+import de.unigoettingen.sub.be.ous.print.util.Util
+import org.junit.Ignore
+import org.junit.Test
+import org.w3c.dom.Document
+
+import de.unigoettingen.sub.be.ous.print.layout.Xml2Parser.LayoutParser
+
+
+@Log4j
+class Xml2ParserTest {
+    static List<URL> URLS = [Xml2ParserTest.getClass().getResource("/layouts-xml/ous40_layout_001_du.asc.xml"), 
+        Xml2ParserTest.getClass().getResource("/layouts-xml/ous40_layout_001_en.asc.xml")]
+    static URL PARSER_XML = Xml2ParserTest.getClass().getResource("/layouts-xml/ous40_layout_001_du.asc.xml")
+    static File SLIPS = new File(Xml2ParserTest.getClass().getResource('/hotfolder/in/').toURI())
+    static List<URL> SLIP_FILES = new ArrayList<URL>()
+    
+    //Build file list
+    @BeforeClass
+    static void setUp () {
+        assertNotNull(SLIPS)
+        def p = ~/.*\.print/
+        SLIPS.eachFileMatch(p) {
+            f ->
+            SLIP_FILES.add(f.toURL())
+            log.info('Added URL ' + f.toURL().toString() + ' to test file list')
+        }
+    }
+    
+    //Test transformations
+    @Test
+    @TypeChecked
+    void testTransform () {
+        for (xml in URLS) {
+            log.info('Transforming XML File ' + xml.toString() + ' to parser')
+            Xml2Parser x2p = new Xml2Parser(xml)
+            try {
+                x2p.transform()
+            } catch (javax.xml.transform.TransformerException te) {
+                log.error('Transformation failed!')
+                fail(te.getMessage())
+            }
+            log.trace('Result:\n----------------START OF RESULT(' + this.getClass().getName() + ')\n' + x2p.getXML())
+            log.trace('----------------END OF RESULT(' + this.getClass().getName() + ')\n')
+        }
+        
+    }
+    
+    //Test if the parsers read files
+    @Test
+    @TypeChecked
+    void testParse() {
+        log.info('Generating Parser for ' + PARSER_XML)
+        Xml2Parser x2p = new Xml2Parser(PARSER_XML)
+        x2p.transform()
+        LayoutParser lp = x2p.getParser()
+        def xslt = new DOMSource(x2p.result)
+        for (slip in SLIP_FILES) {
+            if (!Layout.validateAsc(slip)) {
+                log.warn('File contains invalid characters: ' + slip.toString())
+                continue
+            }
+            def xmlOut = slip.toString().substring(5) + '.xml'
+            lp.parse(slip)
+
+            log.trace('Result:\n----------------START OF RESULT(' + this.getClass().getName() + ')\n' + lp.getXML())
+            log.trace('----------------END OF RESULT(' + this.getClass().getName() + ')\n')
+            log.trace('Saving file to ' + xmlOut)
+            Util.writeDocument(lp.result, new File(xmlOut).toURL())
+               
+        }
+    }
+    
+}
+

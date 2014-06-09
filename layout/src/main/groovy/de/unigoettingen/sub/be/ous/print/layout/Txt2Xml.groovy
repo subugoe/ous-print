ee4dfeca6114fcf47299b09a15ad98a1164757b6
Christian Mahnke <mahnke@sub.uni-goettingen.de>
2014-06-09 07:34:12 +0200

    added stub for TXT to ASC file, not finished yet

---
+++ layout/src/main/groovy/de/unigoettingen/sub/be/ous/print/layout/Txt2Xml.groovy
@@ -0,0 +1,77 @@
+/*
+ * This file is part of the OUS print system, Copyright 2014 SUB Göttingen
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, see http://www.gnu.org/licenses or write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301 USA.
+ */
+
+package de.unigoettingen.sub.be.ous.print.layout
+
+/**
+ *
+ * @author cmahnke
+ */
+class Txt2Xml extends AbstractTransformer {
+    /** The URL of the stylesheet to used */
+    def static URL stylesheet = this.getClass().getResource("/xslt/txt2xml.xsl")
+    
+    //Configuration of Stylesheet
+    /** Parameters of the stylesheet */
+    def static paramPrototypes = ['input': '', 'language': '', 'start-line': '', 'encoding': '']
+            
+    /*
+     * This generates accessor methods for the parameters which live inside of the params Map 
+     */
+    static {
+        paramPrototypes.keySet().each { name ->
+            def methodName = name[0].toUpperCase() + name[1..-1]
+            Txt2Xml.metaClass."set${methodName}" = {String value -> params."${name}" = value }
+            Txt2Xml.metaClass."get${methodName}" = {-> params."${name}" }
+        }
+    }
+    
+    /**
+     * Construts a empty Txt2Xml
+     */
+    Txt2Xml () {
+        
+    }
+    
+    /**
+     * Construts a Txt2Xml, sets and the parameters of the transformation and sets the given input.
+     * @param input the {@link java.net.URL URL} of the document to be transformed
+     * @see #Txt2Xml()
+     */
+    Txt2Xml (URL input) {
+        this()
+        this.input = input
+    }
+    
+    /**
+     * Checks if the required parameters are set and performes the transformation
+     * @throws IllegalStateException if the paramters are empty or not set
+     * @see de.unigoettingen.sub.be.ous.print.layout.AbstractTransformer#transform()
+     */
+    @Override
+    void transform () {
+        if (!validateParams(params)) {
+            throw new IllegalStateException('Params not configured');
+        }
+        log.debug("Using stylesheet " + stylesheet.toString())
+        result = transform(this.input, this.stylesheet, this.params)
+    }
+
+	
+}
+

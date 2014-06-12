521479be9a4ad39f86a28a277fd13f0ce97995a5
Christian Mahnke <mahnke@sub.uni-goettingen.de>
2014-06-12 06:18:34 +0200

    added Camel endpoint

---
+++ layout-component/src/main/groovy/de/unigoettingen/sub/be/ous/print/layout/camel/LayoutEndpoint.groovy
@@ -0,0 +1,55 @@
+/*
+ * This file is part of the OUS Print Server, Copyright 2011, 2012 SUB Göttingen
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, see http://www.gnu.org/licenses or write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301 USA.
+ */
+
+package de.unigoettingen.sub.be.ous.print.layout.camel
+
+import de.unigoettingen.sub.be.ous.print.layout.FORMAT
+
+import groovy.transform.CompileStatic
+import groovy.transform.TypeChecked
+
+import org.apache.camel.Component
+import org.apache.camel.api.management.ManagedAttribute
+import org.apache.camel.impl.ProcessorEndpoint
+import org.apache.camel.spi.UriEndpoint
+import org.apache.camel.spi.UriParam
+
+/**
+ * A simple endpoint for the LayoutProcessor, extending ProcessorEndpoint
+ * @author cmahnke
+ */
+@TypeChecked
+@CompileStatic
+@UriEndpoint(scheme = "layout")
+class LayoutEndpoint extends ProcessorEndpoint {
+    /** The LayoutProcessor to be used */
+    protected LayoutProcessor lp
+    
+    /**
+     * Simple contructor, that calles the conructor of the super class
+     * @param endpointUri, the endpoint URI
+     * @param component, the component
+     * @param lp, the LayoutProcessor
+     */
+    public LayoutEndpoint(String endpointUri, Component component, LayoutProcessor lp) throws Exception {
+        super(endpointUri, component, lp)
+        this.lp = lp
+    }
+  
+}
+
---
+++ layout-component/src/test/groovy/de/unigoettingen/sub/be/ous/print/layout/camel/LayoutEndpointTest.groovy
@@ -0,0 +1,65 @@
+/*
+ * This file is part of the OUS Print Server, Copyright 2011, 2012 SUB Göttingen
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, see http://www.gnu.org/licenses or write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301 USA.
+ */
+
+package de.unigoettingen.sub.be.ous.print.layout.camel
+
+import org.apache.camel.CamelContext
+import org.apache.camel.EndpointInject
+import org.apache.camel.builder.RouteBuilder
+import org.apache.camel.component.mock.MockEndpoint
+import org.apache.camel.impl.DefaultCamelContext
+import org.apache.camel.test.junit4.CamelTestSupport
+
+import org.junit.Before
+import org.junit.Test
+
+/**
+ * Also uses the Endpoint URI, not the processor
+ * @author cmahnke
+ */
+class LayoutEndpointTest  extends CamelTestSupport  {
+    static URL PARSER_TXT = LayoutProcessorTest.getClass().getResource("/layouts/ous40_layout_001_du.asc")
+    static URL FO = LayoutProcessorTest.getClass().getResource("/xslt/layout2fo.xsl")
+    
+    @EndpointInject(uri = "mock:result")
+    protected MockEndpoint resultEndpoint
+    
+    @Test
+    public void testMessageCount() {
+        //We've got 36 Test files - This takes some time
+        resultEndpoint.setMinimumResultWaitTime(5000)
+        resultEndpoint.setResultWaitTime(30000)
+        resultEndpoint.expectedMessageCount(36)
+        assertMockEndpointsSatisfied();       
+    }
+    
+    @Override
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("file:./target/generated-test-resources/hotfolder/in?include=.*.print&noop=true")
+                .to("layout:.?xslfo=./target/test-classes/xslt/layout2fo.xsl&template=./target/test-classes/layouts/ous40_layout_001_du.asc")
+                .to('file:./target/?fileName=${file:name}.pdf')
+                .to("mock:result");
+            }
+        };
+    }
+
+}
+

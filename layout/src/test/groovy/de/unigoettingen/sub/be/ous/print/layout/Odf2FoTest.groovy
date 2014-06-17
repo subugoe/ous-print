d0cfef810025ddd20fded4ee7acbeb49484c8076
Christian Mahnke <mahnke@sub.uni-goettingen.de>
2014-06-17 09:50:09 +0200

    added unit test for conversion of ODF into XSL-FO

---
+++ layout/src/test/groovy/de/unigoettingen/sub/be/ous/print/layout/Odf2FoTest.groovy
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package de.unigoettingen.sub.be.ous.print.layout
+
+import de.unigoettingen.sub.be.ous.print.layout.Xml2Parser.LayoutParser
+import groovy.transform.TypeChecked
+
+import groovy.util.logging.Log4j
+import org.junit.BeforeClass
+import org.junit.Test
+
+/**
+ * Unit tests for converting ODf into XSL-FO
+ * @author cmahnke
+ */
+@Log4j
+class Odf2FoTest {
+    static URL LAYOUT = Layout2FoTest.getClass().getResource("/layouts-xml/ous40_layout_001_du.asc.xml")
+    static URL ODF = Layout2FoTest.getClass().getResource("/odf/Bestellschein%20Template.odf")
+    static LayoutParser LP
+    static File SLIPS = new File(Xml2ParserTest.getClass().getResource('/hotfolder/in/').toURI())
+    static List<URL> SLIP_FILES = new ArrayList<URL>()
+    
+    @BeforeClass
+    static void setUp () {
+        //Set Up parser
+        log.info('Generating Parser for ' + LAYOUT)
+        Xml2Parser x2p = new Xml2Parser(LAYOUT)
+        x2p.transform()
+        LP = x2p.getParser()
+        assertNotNull(ODF)
+        // Slip files
+        assertNotNull(SLIPS)
+        def p = ~/.*\.print/
+        SLIPS.eachFileMatch(p) {
+            f ->
+            SLIP_FILES.add(f.toURL())
+            log.info('Added URL ' + f.toURL().toString() + ' to test file list')
+        }
+    }
+    
+    @Test
+    @TypeChecked
+    void testFormatFo() {
+        for (slip in SLIP_FILES) {
+            if (!Layout.validateAsc(slip)) {
+                log.warn('File contains invalid characters: ' + slip.toString())
+                continue
+            }
+        
+            LP.parse(slip)
+            Odf2Fo o2f = new Odf2Fo(LP.result, ODF)
+            o2f.transform()
+            log.trace('Result:\n----------------START OF RESULT(' + this.getClass().getName() + ')\n' + o2f.getXML())
+            log.trace('----------------END OF RESULT(' + this.getClass().getName() + ')\n')
+        }
+    }
+	
+}
+

d0762e5f32c6cc78f264b17a67dd9575f1a7e75c
Christian Mahnke <mahnke@sub.uni-goettingen.de>
2014-06-17 12:43:16 +0200

    added license

--- layout/src/test/groovy/de/unigoettingen/sub/be/ous/print/layout/Odf2FoTest.groovy
+++ layout/src/test/groovy/de/unigoettingen/sub/be/ous/print/layout/Odf2FoTest.groovy
@@ -1,7 +1,19 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * This file is part of the OUS Print Server, Copyright 2011, 2012 SUB Göttingen
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, see http://www.gnu.org/licenses or write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301 USA.
  */
 
 package de.unigoettingen.sub.be.ous.print.layout

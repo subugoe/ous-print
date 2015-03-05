ccfaccf1a0deaf857d0a8252e79450a790cdb62a
Christian Mahnke <cmahnke@gmail.com>
2015-03-05 14:48:40 +0100

    Added constructor for encoding

--- layout/src/main/groovy/de/unigoettingen/sub/be/ous/print/layout/Layout.groovy
+++ layout/src/main/groovy/de/unigoettingen/sub/be/ous/print/layout/Layout.groovy
@@ -51,95 +51,115 @@ import de.unigoettingen.sub.be.ous.print.util.Util
 class Layout {
     /** The defaul page size */
     static PageSize DEFAULT_PAGE_SIZE = PageSize.A4
-    
+
     /** The default encoding for files to be read */
     static String DEFAULT_ENCODING = 'Cp850'
-    
+
     /** The input FORMAT */
     FORMAT inFormat
-    
+
     /** The {@link java.io.InputStream InputStream} to consume */
     InputStream input
-    
+
+    /** The encoding to decode the stream */
+    String encoding
+
     /** The desired output FORMAT*/
     FORMAT outFormat
-    
+
     /** The {@link java.io.OutputStream OutputStream} to write results to */
     OutputStream output
-    
+
     /** The {@link java.net.URL URL} of the location of the XSL-FO file */
     URL xslfo
-    
+
     /** The {@link java.net.URL URL} of the path to search for files referenced in the XSL-FO file */
     URL includePath
-    
+
     /** The {@link java.net.URL URL} of the location of the parser (can also be an ASC template) */
     URL parser
-    
+
     /** Whether the layouter should work in debug mode */
     Boolean debug = false
-    
+
     /** Parameters for the XSLT transformation */
     Map<String, String> params = [:]
-    
+
     /** The desired page size of the result */
     PageSize pageSize = DEFAULT_PAGE_SIZE
-    
+
     //The converters, these are defined at class level to get their content for debugging.
     /** Internal property of the LayoutParser to be used, used for debugging */
     LayoutParser lp
-    
+
     /** Internal property of the Layout2Fo to be used, used for debugging */
     Layout2Fo l2f
-    
+
     /** Internal property of the Xml2Parser to be used, used for debugging */
     Xml2Parser x2p
-    
+
     /**
      * Creates a Layout object
      * @param inFormat the input FORMAT
      * @param input the InputStream to read
      * @param outFormat the output FORMAT
      * @param output the {@link java.io.OutputStream OutputStream}
-     * @param xslfo, {@link java.net.URL URL} of the XLS-FO file to use
-     * @param includePath, {@link java.net.URL URL} of the include path for references inside the XSL FO
-     * @param parser, {@link java.net.URL URL} of the parser to use
+     * @param xslfo , {@link java.net.URL URL} of the XLS-FO file to use
+     * @param includePath , {@link java.net.URL URL} of the include path for references inside the XSL FO
+     * @param parser , {@link java.net.URL URL} of the parser to use
+     * @param encoding , encoding to decode the input Stream
      */
     @TypeChecked
-    Layout (FORMAT inFormat, InputStream input, FORMAT outFormat, OutputStream output, URL xslfo, URL includePath, URL parser) {
+    Layout(FORMAT inFormat, InputStream input, FORMAT outFormat, OutputStream output, URL xslfo, URL includePath, URL parser, String encoding) {
         this.inFormat = inFormat
         this.input = input
         this.outFormat = outFormat
         if (output != null) {
             this.output = output
         } else {
-            this.output = new ByteArrayOutputStream() 
+            this.output = new ByteArrayOutputStream()
         }
-        
+
         this.xslfo = xslfo
         this.includePath = includePath
-        this.parser = parser 
+        this.parser = parser
+        this.encoding = encoding
     }
-    
+
+    /**
+     * Creates a Layout object, assuming the right encoding
+     * @param inFormat the input FORMAT
+     * @param input the InputStream to read
+     * @param outFormat the output FORMAT
+     * @param output the {@link java.io.OutputStream OutputStream}
+     * @param xslfo , {@link java.net.URL URL} of the XLS-FO file to use
+     * @param includePath , {@link java.net.URL URL} of the include path for references inside the XSL FO
+     * @param parser , {@link java.net.URL URL} of the parser to use
+     */
+    @TypeChecked
+    Layout(FORMAT inFormat, InputStream input, FORMAT outFormat, OutputStream output, URL xslfo, URL includePath, URL parser) {
+        this(inFormat, input, outFormat, output, xslfo, includePath, parser, DEFAULT_ENCODING)
+    }
+
     /**
      * Starts the layouting, writes to the given {@link java.io.OutputStream OutputStream}
      */
-    def layout () {
+    def layout() {
         //TODO: Finish other in and outputs
         //Create Parser XML File
         if (parser != null) {
             lp = getLayoutParser(parser)
             //Layout parser should be ready
             if (inFormat == FORMAT.TEXT || inFormat == FORMAT.ASC) {
-                lp.parse(input)
+                lp.parse(input, encoding)
             }
         }
-        
+
         if (outFormat == FORMAT.PDF || outFormat == FORMAT.XSLFO || outFormat == FORMAT.PS) {
             if (xslfo == null) {
                 throw new IllegalArgumentException('No XSL-FO set for output of formated Layout')
             }
-            
+
             log.trace('Writing PDF or FO file')
             l2f = new Layout2Fo(lp.result, xslfo)
             if (debug) {
@@ -163,66 +183,66 @@ class Layout {
             output.write(lp.getXML().getBytes())
         }
     }
-    
+
     /**
      * Validates a given {@link java.net.URL URL} if the file contains invalid unicode characters
      * @returns Boolean if the file is valid or not
      */
     @TypeChecked
-    def static Boolean validateAsc (URL input) {
+    def static Boolean validateAsc(URL input) {
         def file = new File(input.toURI())
         if (file.text =~ /[\x10-\x1f]/) {
             return false
         }
         return true
     }
-    
+
     /**
      * Returns the contents of a given {@link java.net.URL URL} in the requested encoding
      * @return String the contents (converted from code page 850 into UTF-8) of the given URL
      */
     @TypeChecked
-    def static String readFile (URL input, String encoding) {
+    def static String readFile(URL input, String encoding) {
         //See http://docs.oracle.com/javase/8/docs/technotes/guides/intl/encoding.doc.html
         //See http://www.torsten-horn.de/techdocs/encoding.htm
         return readFile(input.openStream(), encoding)
     }
-    
+
     /**
      * Returns the contents of a given {@link java.io.InputStream InputStream} in the requested encoding
      * @return String the contents (converted from code page 850 into UTF-8) of the given InputStream
      */
     @TypeChecked
-    def static String readFile (InputStream input, String encoding) {
+    def static String readFile(InputStream input, String encoding) {
         //See http://docs.oracle.com/javase/8/docs/technotes/guides/intl/encoding.doc.html
         //See http://www.torsten-horn.de/techdocs/encoding.htm
         return input.getText(encoding)
     }
-    
+
     /**
      * Converts a {@link java.net.URL URL} object into a file object
      * @returns {@link java.io.File File} representing the given {@link java.net.URL URL}
      */
     @TypeChecked
-    def static File URLToFile (URL url) {
+    def static File URLToFile(URL url) {
         //taken from https://weblogs.java.net/blog/kohsuke/archive/2007/04/how_to_convert.html
         File f;
         try {
             f = new File(url.toURI())
-        } catch(URISyntaxException e) {
+        } catch (URISyntaxException e) {
             log.trace('Cought URISyntaxException', e)
             f = new File(url.getPath())
         }
         return f
-    } 
-    
+    }
+
     /**
      * Guesses the content (Mime) type of the given {@link java.net.URL URL}, uses Apache Tika
      * @param {@link java.net.URL URL} of the file
-     * @returns String, containing the Mime type
+     * @returns String , containing the Mime type
      */
     @TypeChecked
-    def static String guessContentType (URL url) {
+    def static String guessContentType(URL url) {
         File file = URLToFile(url)
         AutoDetectParser parser = new AutoDetectParser()
         parser.setParsers(new HashMap<MediaType, Parser>())
@@ -236,13 +256,13 @@ class Layout {
 
         return metadata.get(HttpHeaders.CONTENT_TYPE)
     }
-    
+
     /**
      * Internal function to get a LayoutParser for the given {@link java.net.URL URL}. Detects the Mime
      * type of the file and can create LayoutParser objects for TXT (ASC) and XML
      * representations
-     * 
-     * @param parser, the {@link java.net.URL URL} of the layout representation
+     *
+     * @param parser , the {@link java.net.URL URL} of the layout representation
      * @returns a LayoutParser object
      */
     @TypeChecked
@@ -269,13 +289,13 @@ class Layout {
         }
         return lp
     }
-    
+
     /**
      * Internal function to get a Layout2Fo for the given {@link java.net.URL URL}.
      * This can either be a XSL-FO file or a ODD file
-     * 
-     * @param parser, the {@link java.net.URL URL} of the XSL-FO ODF source
-     * @param doc, the {@link org.w3c.dom.Document Document} to be transformed
+     *
+     * @param parser , the {@link java.net.URL URL} of the XSL-FO ODF source
+     * @param doc , the {@link org.w3c.dom.Document Document} to be transformed
      * @returns a Layout2Fo object
      * @throws IllegalStateException if mime type is not supported
      */
@@ -292,14 +312,14 @@ class Layout {
         }
         return l2f
     }
-    
+
     //Utility Functions
     /**
      * Resolves relative file paths
-     * @param path, String the path to resolve
+     * @param path , String the path to resolve
      * @returns {@link java.io.File File} the absolute File
      */
-    public static File getFile (String path) {
+    public static File getFile(String path) {
         String prefix = ''
         if (!path.startsWith('/')) {
             prefix = System.getProperty("user.dir") + File.separator
@@ -307,16 +327,16 @@ class Layout {
         File f = new File(prefix + path)
         return f
     }
-    
+
     /**
      * Resolves relative file paths
-     * @param path, String the path to resolve
+     * @param path , String the path to resolve
      * @returns a absolute {@link java.net.URL URL}
      */
-    public static URL getURL (String path) {
+    public static URL getURL(String path) {
         return getFile(path).toURI().toURL()
     }
-    
+
     /**
      * A Enum representing page sizes A4 and A5, providing some JPS constants
      */
@@ -324,17 +344,17 @@ class Layout {
         A4('A4'), A5('A5')
         /** The name of this PageSize */
         String name
-        
+
         /**
          * Constructor, sets up a PageSize object
          */
-        PageSize(String name) { 
-            this.name = name 
+        PageSize(String name) {
+            this.name = name
         }
         /**
          * Get a PageSize for a given String
          * @returns PageSize the format or null
-         */ 
+         */
         public static PageSize fromString(String size) {
             if (size != null) {
                 for (PageSize ps : values()) {
@@ -345,12 +365,12 @@ class Layout {
             }
             return null
         }
-        
+
         /**
          * Returns the MediaSizeName for the given PageSize
          * @returns {@link javax.print.attribute.standard.MediaSizeName MediaSizeName}, the name of the media size
          */
-        public static MediaSizeName getMediaSizeName (PageSize ps) {
+        public static MediaSizeName getMediaSizeName(PageSize ps) {
             if (ps == A4) {
                 return MediaSizeName.ISO_A4
             } else if (ps == A5) {
@@ -359,12 +379,12 @@ class Layout {
                 reurn null
             }
         }
-        
+
         /**
          * Return the OrientationRequested for the given PageSize
          * @returns {@link javax.print.attribute.standard.OrientationRequested OrientationRequested}, the requested orientation
          */
-        public static OrientationRequested getOrientationRequested (PageSize ps) {
+        public static OrientationRequested getOrientationRequested(PageSize ps) {
             if (ps == A4) {
                 return OrientationRequested.PORTRAIT
             } else if (ps == A5) {
@@ -373,7 +393,7 @@ class Layout {
                 return null
             }
         }
-        
+
         /**
          * Return the Paper for the given PageSize
          * @returns {@link java.awt.print.Paper Paper}, the paper
@@ -389,12 +409,12 @@ class Layout {
                 paper = new Paper()
                 paper.setSize(420, 595)
                 //A5 (borderless)
-                paper.setImageableArea(0,0,334,509)
+                paper.setImageableArea(0, 0, 334, 509)
             }
             return paper
         }
-        
+
     }
-    
+
 }
 

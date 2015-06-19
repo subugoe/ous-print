7cc4034c2c230c290065f33c028d433c3ae03fc8
Christian Mahnke <cmahnke@gmail.com>
2015-06-19 10:25:46 +0200

    added license header

--- layout-component/src/test/groovy/de/unigoettingen/sub/be/ous/print/layout/camel/ConcatAggregationStrategyXSLTTest.groovy
+++ layout-component/src/test/groovy/de/unigoettingen/sub/be/ous/print/layout/camel/ConcatAggregationStrategyXSLTTest.groovy
@@ -1,3 +1,21 @@
+/*
+ * This file is part of the OUS Print Server, Copyright 2015 SUB Göttingen
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, see http://www.gnu.org/licenses or write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301 USA.
+ */
+
 package de.unigoettingen.sub.be.ous.print.layout.camel
 
 import org.apache.camel.EndpointInject
